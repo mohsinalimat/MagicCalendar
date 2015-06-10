4c2883c3ca50a5891d3adc9b74f850441be411cd
matbeich <matbeich@ya.ru>
2015-06-10 17:16:13 +0300

    currency added

---
+++ MagicCalendar/MCParserHelper.h
@@ -0,0 +1,13 @@
+//
+//  MCParserHelper.h
+//  MagicCalendar
+//
+//  Created by Bogdan Matveev on 10.06.15.
+//  Copyright (c) 2015 BOGDAN MATVEEV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCParserHelper : NSObject
+
+@end
---
+++ MagicCalendar/MCParserHelper.m
@@ -0,0 +1,13 @@
+//
+//  MCParserHelper.m
+//  MagicCalendar
+//
+//  Created by Bogdan Matveev on 10.06.15.
+//  Copyright (c) 2015 BOGDAN MATVEEV. All rights reserved.
+//
+
+#import "MCParserHelper.h"
+
+@implementation MCParserHelper
+
+@end
